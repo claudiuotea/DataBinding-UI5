b48fb999215cf41a7b7f15be9ae77f50bb2a51f4
Claudiu Otea <claudiu_otea@yahoo.com>
2021-10-20 14:11:54 +0300

    Formatting values

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,22 @@
-sap.ui.define([
-  "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller,MessageToast) {
-  "use strict";
-  return Controller.extend("sap.ui.demo.databinding.controller.App", {
-    resetFirstName : function () {
-      MessageToast.show(this.getView().getModel().setProperty("/firstName","Woops!"));
-    }
-  });
-});
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/m/library"],
+  function (Controller, MessageToast, mobileLibrary) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.databinding.controller.App", {
+      resetFirstName: function () {
+        MessageToast.show(
+          this.getView().getModel().setProperty("/firstName", "Woops!")
+        );
+      },
+
+      formatMail: function (sFirstName, sLastName) {
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        return mobileLibrary.URLHelper.normalizeEmail(
+          sFirstName + "." + sLastName + "@example.com",
+          oBundle.getText("mailSubject", [sFirstName]),
+          oBundle.getText("mailBody")
+        );
+      },
+    });
+  }
+);
